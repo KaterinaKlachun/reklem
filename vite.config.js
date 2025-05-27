279d108e8f88cf38839d96ec26d93fc6d0c2e6bd
Katerina Klachun <katerina.klachun@mail.ru>
2025-05-27 22:40:07 +1000

    деплой 27.05.25

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-    base: '/build/', // важно для корректной работы ссылок в продакшене
+    base: 'https://reklem.onrender.com/build/',
 
     plugins: [
         laravel({
