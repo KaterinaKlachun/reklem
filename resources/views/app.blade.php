592a084e4e2d3682361f0fb690741446fa5f9f75
Katerina Klachun <katerina.klachun@mail.ru>
2025-05-29 16:30:14 +1000

    деплой 29.05.25

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self' https: 'unsafe-inline' 'unsafe-eval'; img-src 'self' https: data:; font-src 'self' https: data:;">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-    base: 'https://reklem.onrender.com/build/',
+    base: '/build/',
 
     plugins: [
         laravel({
