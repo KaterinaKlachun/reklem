47a639d067eaa7d37717131cc74505a655dc0b33
Katerina Klachun <kataklachun@mail.ru>
2025-06-02 21:59:50 +1000

    обновление 4 02.06.25

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
 window.axios = axios;
 
+window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL || 'https://reklem.onrender.com';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
