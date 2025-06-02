6dd25f8876ff6584712773c7b728b350826e8798
Katerina Klachun <kataklachun@mail.ru>
2025-06-02 21:42:51 +1000

    обновление 4 02.06.25

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self' http: https: 'unsafe-inline' 'unsafe-eval'; style-src 'self' http: https: 'unsafe-inline'; script-src 'self' http: https: 'unsafe-inline' 'unsafe-eval'; img-src 'self' http: https: data:; font-src 'self' http: https: data:; connect-src 'self' http: https:; style-src-elem 'self' http: https: 'unsafe-inline'; script-src-elem 'self' http: https: 'unsafe-inline' 'unsafe-eval';">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self' https: 'unsafe-inline' 'unsafe-eval'; style-src 'self' http: https: 'unsafe-inline'; script-src 'self' http: https: 'unsafe-inline' 'unsafe-eval'; img-src 'self' http: https: data:; font-src 'self' http: https: data:; connect-src 'self' http: https:; style-src-elem 'self' http: https: 'unsafe-inline'; script-src-elem 'self' http: https: 'unsafe-inline' 'unsafe-eval';">
     <meta http-equiv="X-Content-Type-Options" content="nosniff">
     <meta http-equiv="X-XSS-Protection" content="1; mode=block">
     <meta name="referrer" content="strict-origin-when-cross-origin">
